4f66566a477301d95d956ea09ea5a9627fb792d2
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-11-18 12:45:58 +0000

    added questions and belongs_to quiz

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118120141) do
+ActiveRecord::Schema.define(version: 20131118123819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "quiz_id"
+  end
 
   create_table "quizzes", force: true do |t|
     t.string   "title"
