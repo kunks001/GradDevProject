f78bdbf8f1afaa62bc2231a7755833c2c1d1cecc
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-11-18 10:47:20 +0000

    Initial commit, configured database config file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GradDevProject::Application.config.secret_key_base = '2603b75bc5d9e67cf3127fca20e4d17c49de800654b33a60e0a6d1ecf928cbc7e32eb92f695f21e5aeaeb4f04732a702c911854c421000249376d552b5a59e3e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GradDevProject::Application.config.session_store :cookie_store, key: '_GradDevProject_session'
