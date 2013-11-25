a1f5279bfe5efa69122588b9d6b60e206cf92aa3
Jonathan Tsang <chewymeister88@googlemail.com>
2013-11-25 12:26:25 +0000

    commented out user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,42 +1,41 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe User do
+# describe User do
+#   let(:user) { User.new(email: 'deniss@deniss.com') }
 
-	let(:user) { User.new(email: 'deniss@deniss.com') }
+#   subject{user}
 
-	subject{user}
+#   it { expect(user).to respond_to 'email' }
+#   it { expect(user).to be_valid }
 
-	it { expect(user).to respond_to 'email' }
-	it { expect(user).to be_valid }
+#   describe "email" do
 
-	describe "email" do
-		
-		it "should not be empty" do
-			user.email = ''
-			user.should_not be_valid
-		end
+#     it "should not be empty" do
+#       user.email = ''
+#       user.should_not be_valid
+#     end
 
-		context "when formatted incorrectly" do
+#     context "when formatted incorrectly" do
 
-			it "should not be valid" do
+#       it "should not be valid" do
+#         emails = %w[userdeniss.com deniss@.com deniss@makers deniss@makers.]
+#         emails.each do |invalid_email|
+#           user.email = invalid_email
+#           expect(user).not_to be_valid
+#         end
+#       end
+#     end
+#   end
 
-				emails = %w[userdeniss.com deniss@.com deniss@makers deniss@makers.]
-				emails.each do |invalid_email|
-					user.email = invalid_email
-					expect(user).not_to be_valid
-				end
-			end
-		end
+#   context "when formatted correctly" do
+#     it 'should be valid' do
 
-		context "when formatted correctly" do
-			it 'should be valid' do
+#       emails = %w[user@outlook.com UsEr@deniss.com users@denISs.co.uk deniss@gmail.com]
+#       emails.each do |valid_email|
+#         user.email = valid_email
+#         expect(user).to be_valid
+#       end
 
-				emails = %w[user@outlook.com UsEr@deniss.com users@denISs.co.uk deniss@gmail.com]
-				emails.each do |valid_email|
-					user.email = valid_email
-					expect(user).to be_valid
-				end
-			end
-		end
-	end
-end
+#     end
+#   end
+# end
