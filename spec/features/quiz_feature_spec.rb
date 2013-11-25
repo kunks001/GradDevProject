4f5fa4aedbaebec18fd7427ec3e4f81d02db7799
Jonathan Tsang <chewymeister88@googlemail.com>
2013-11-25 12:37:00 +0000

    Renamed contexts and describes for quiz_feature_spec.rb

--- spec/features/quiz_feature_spec.rb
+++ spec/features/quiz_feature_spec.rb
@@ -1,34 +1,30 @@
 require 'spec_helper'
-describe 'the quizzes section' do
-  # before(:all) do
-    let!(:quiz) { FactoryGirl.create (:quiz) }
-  # end
+describe 'Quizzes' do
+  let!(:quiz) { FactoryGirl.create (:quiz) }
 
-  describe '/quizzes' do
-    it 'should display quizzes' do
-      visit '/quizzes'
+  describe 'When the user visits the homepage' do
+    it 'the user should see a list of quiz titles' do
+      visit '/'
 
       expect(page).to have_content 'Quiz 1'
     end
   end
 
-  describe '/quizzes/:id' do
-    it 'has its own page' do
+  describe 'When the user clicks on the title link' do
+    it 'the user is redirected to the unique quiz page' do
       visit '/quizzes'
-      # raise page.html
       click_link 'Quiz 1'
 
       expect(page).to have_css 'h1', text: 'Quiz 1'
     end
   end
 
-  describe '/quizzes/new' do
+  context 'A quiz is valid when' do
     
-    it 'creates a new quiz' do
+    it 'a title is provided' do
       visit '/quizzes/new'
 
       within '.new_quiz' do
-        # raise page.html
 
         fill_in 'Title', with: 'Brand new quiz'
         click_button "Create Quiz"
@@ -38,9 +34,8 @@ describe 'the quizzes section' do
       expect(page).to have_content 'Brand new quiz'
     end
 
-    it 'can also create a new question' do
+    it 'A title and a question are provided' do
       visit '/quizzes/new'
-      # raise page.html
 
       fill_in 'Title', with: 'Quiz title'
       fill_in 'Question 1', with: 'What is your name?'
@@ -49,15 +44,14 @@ describe 'the quizzes section' do
       expect(page).to have_content 'What is your name?'
       expect(Quiz.last.questions).not_to be_empty
     end
+  end 
 
-    it 'should not accept a new quiz without a title' do
+  context 'A quiz is not valid when' do
+    it 'A title is not provided' do
       visit '/quizzes/new'
       click_button 'Create Quiz'
 
       expect(page).to have_content 'error'
     end
-
-  end 
+  end
 end
-
-# end
