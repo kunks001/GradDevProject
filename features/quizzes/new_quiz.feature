1c6fe1ed748095a4fa573ec31b7902178fdccfbd
chewymeister <chewymeister88@googlemail.com>
2013-12-09 18:24:24 +0000

    Completed new_quiz.feature with data table

--- features/quizzes/new_quiz.feature
+++ features/quizzes/new_quiz.feature
@@ -4,11 +4,11 @@ Feature: Admin user is able to create a new quiz that contains questions and ans
     Given the user visits the new quiz path
     When the user enters the following data into the new quiz form
 
-    | Quiz title | Question                    | Answers | Correct? |
-    | Test       | What is capital of England? | London  | true     |
-    |            |                             | Paris   | false    |
-    |            |                             | Berlin  | false    |
-    |            |                             | Rome    | false    |
+    | Quiz title | Test                       |       |        |       |
+    | Question   | What is capital of England?|       |        |       | 
+    | Answers    | London                     | Paris | Berlin | Rome  |
+    | Correct?   | true                       | false | false  | false |
+
     
     Then the number of quizzes will go up by one
     And the user will be able to see the title "Test" on the root page.
--- features/step_definitions/new_quiz_steps.rb
+++ features/step_definitions/new_quiz_steps.rb
@@ -3,23 +3,29 @@ Given(/^the user visits the new quiz path$/) do
 end
 
 When(/^the user enters the following data into the new quiz form$/) do |table|
-  @data = table.hashes
-  # puts @title.inspect
-  raise page.html
-  @data.each do |hash|
-    hash.each do |key, value|
-      fill_in 'quiz_title', with: value if value
-
+  data = table.raw
+  quiz_title = data[0][1]
+  question = data[1][1]
+  responses = data[2][1..4]
+  correctness = data[3][1..4]
+  answers = Hash[responses.zip(correctness)]
+  fill_in :quiz_title, with: quiz_title
+  within_fieldset('question_1') do
+    fill_in 'Question 1', with: question
+    page.all('Answer').zip(answers) do |page_elem, answer|
+      page_elem.fill_in 'Answer', with: answer[0]
+      page_elem.choose 'Correct?' if answer[1] == 'true'
     end
   end
-
-  puts @data.inspect
+  
+  click_button 'Create Quiz'
 end
 
 Then(/^the number of quizzes will go up by one$/) do
-
+  expect(Quiz.count).to eq 1
 end
 
 Then(/^the user will be able to see the title "(.*?)" on the root page\.$/) do |title|
-  # expect(page).to have_content title
+  expect(page).to have_content title
+  expect(page).to have_content 'What is capital of England?'
 end
