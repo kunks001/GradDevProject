6d6b8349be23541cd98ca6003597da984f866c06
Srikanth <srikanth.kunkulagunta@gmail.com>
2014-01-05 13:24:45 +0000

    switched to dev branch

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -8,7 +8,6 @@ class AttemptsController < ApplicationController
     @quiz = Quiz.find params[:quiz_id]
     answer_ids = params[:answer_ids].values
     scorer = Scorer.new(@quiz)
-
     final_score = scorer.total(answer_ids)
     render text: "#{final_score}%"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -37,4 +37,10 @@ ActiveRecord::Schema.define(version: 20131122143229) do
     t.datetime "updated_at"
   end
 
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
